9bfa43bf1a9ecfe7062e5dd22022544aa2dd494a
Simeon Vutov <simeon32.dev@gmail.com>
2025-11-27 05:24:38 +0100

    Added solution for Exercise 2 for cpp. Also fixed exercise 1 because it did not check for empty arr

--- practicalLACPP/src/cpp/your_code_here.h
+++ practicalLACPP/src/cpp/your_code_here.h
@@ -13,6 +13,10 @@
 // ==================================
 std::pair<float, float> statistics(std::span<const float> values)
 {
+    if (values.empty()) {
+        return { 0.0f, 0.0f };
+    }
+
     float mean = 0.0f;
     for(float v : values) {
         mean += v;
@@ -37,10 +41,23 @@ struct Tree {
     float value;
     std::vector<Tree> children;
 };
+
+float countRecursive(const Tree &node, int level, bool countOnlyEvenLevels) {
+    float sum = 0.0f;
+
+    if(!countOnlyEvenLevels || (level % 2 ==0)) {
+        sum += node.value;
+    }
+
+    for(const auto &child : node.children) {
+        sum += countRecursive(child, level + 1, countOnlyEvenLevels);
+    }
+
+    return sum;
+}
 // Your implementation goes here. Feel free to define helper functions/structs/classes in this
 // file if you need to. Make sure to put them above this function to prevent linker warnings.
 float countTree(const Tree& tree, bool countOnlyEvenLevels)
 {
-    // Your solution goes here
-    return 0.0f;
+    return countRecursive(tree, 0, countOnlyEvenLevels);
 }
