420841e154f2b446706cab76fff169854f43c102
Simeon Vutov <simeon32.dev@gmail.com>
2025-12-14 20:37:12 +0100

    Added the solution for maze assignment

--- Transformations/maze/src/your_code_here_maze.h
+++ Transformations/maze/src/your_code_here_maze.h
@@ -29,6 +29,26 @@ enum class MovementCommand : int {
 inline glm::mat3 computeMazeTransformation(std::span<const MovementCommand> moves)
 {
     glm::mat3 out = glm::identity<glm::mat3>();
+    
+    out = glm::translate(out, glm::vec2(1.0f, 1.0f));
+
+    for(const MovementCommand &move: moves) {
+        switch(move) {
+            case MovementCommand::MoveForward:
+                out = glm::translate(out, glm::vec2(0.0f, 1.0f));
+                break;
+            case MovementCommand::RotateLeft:
+                out = glm::translate(out, glm::vec2(0.5f, 0.5f));
+                out = glm::rotate(out, glm::half_pi<float>());
+                out = glm::translate(out, -glm::vec2(0.5f, 0.5f));
+                break;
+            case MovementCommand::RotateRight:
+                out = glm::translate(out, glm::vec2(0.5f, 0.5f));
+                out = glm::rotate(out, -glm::half_pi<float>());
+                out = glm::translate(out, -glm::vec2(0.5f, 0.5f));
+                break;
+        }
+    }
 
     return out;
 }
